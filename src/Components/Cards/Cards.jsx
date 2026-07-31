0c65f852cbd6301a9f68353183a831d2e8a9eb1b
Jafar610 <jafarmohamed955@gmail.com>
2026-07-31 22:47:41 +0300

    final commit

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -4,14 +4,14 @@ import images from "./Card";
 function Cards() {
   const { img1, img2, img3, img4 } = images;
   return (
-    <div className="bg-white py-8 ">
-      <h1 className="py-3 px-25 text-2xl font-bold">MOST INTERESTING</h1>
+    <div className="bg-white py-8">
+      <h1 className="py-3 px-4 text-2xl font-bold sm:px-6">MOST INTERESTING</h1>
 
-      <div className="flex justify-center align-center gap-8 w-full h-80 pb-6">
-        <img src={img1} alt="" className="w-65" />
-        <img src={img2} alt="" className="w-65" />
-        <img src={img3} alt="" className="w-65" />
-        <img src={img4} alt="" className="w-65" />
+      <div className="grid gap-4 px-4 pb-6 sm:grid-cols-2 lg:grid-cols-4 lg:px-6">
+        <img src={img1} alt="" className="w-full rounded-2xl object-cover shadow-sm" />
+        <img src={img2} alt="" className="w-full rounded-2xl object-cover shadow-sm" />
+        <img src={img3} alt="" className="w-full rounded-2xl object-cover shadow-sm" />
+        <img src={img4} alt="" className="w-full rounded-2xl object-cover shadow-sm" />
       </div>
     </div>
   );
